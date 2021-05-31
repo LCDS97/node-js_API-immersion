6e6eae1910f7ad59eb2220a6174b31b9f545df1f
Lucas Conceição dos Santos <lucas.cds1997@gmail.com>
2021-05-31 00:36:18 -0300

    Manipulating list with for, forIn and forOf

--- manipulating-lists/for-forin/1-for-forin-forof.js
+++ manipulating-lists/for-forin/1-for-forin-forof.js
@@ -7,28 +7,32 @@ async function main() {
        const result = await service.obterPessoas('a')
        const names = []
 // NOTE Antiga maneira para fazer um incremento de for, iremos utilizar o for in e for of
-/*        console.time('for')
+       console.time('for')
        for(let i=0; i <= result.results.length - 1; i++) {
            const pessoa = result.results[i]
            names.push(pessoa.name)
        }
-       console.timeEnd('for') */
+       console.timeEnd('for')
        
 // NOTE Com for in mesmo objetivo, mas com menos linha de código
-/*         console.time('forin')       
+        console.time('forin')       
         for(let i in result.results) {
            const pessoa = result.results[i]
            names.push(pessoa.name)
         }
-        console.timeEnd('forin') */
-    
+        console.timeEnd('forin')
+    console.time('forof')
     for (pessoa of result.results) {
         names.push(pessoa.name)
     }
-       console.log('names', names)
+    console.timeEnd('forof')
+    console.log('names', names)
+
     } catch (error) {
         console.error('Erro interno', error);
     }
+
+    
 }
 
 main()
